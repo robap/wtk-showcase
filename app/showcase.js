fa034f9841a199e85a39936e4043098252a417b2
Rob Apodaca <rob.apodaca@gmail.com>
2011-03-12 20:57:30 -0600

    make use of the additions to wtk

--- app/showcase.js
+++ app/showcase.js
@@ -15,52 +15,97 @@
 goog.provide('showcase');
 
 goog.require('goog.dom');
-goog.require('goog.ui.Button');
+goog.require('wtk.Button');
 goog.require('wtk.ThemeSwitcher');
 goog.require('wtk.Dialog');
+goog.require('wtk.menubar.Menubar');
+goog.require('wtk.menubar.Menu');
+goog.require('wtk.menubar.MenuItem');
+goog.require('wtk.util.Command');
+goog.require('wtk.util.KeyboardShortcut');
 
 /**
  * @constructor
  */
 showcase = function(parameters) {
-  this.parameters_ = {};
-  this.parameters_.containerId = parameters['containerId'] || 'showcase';
-  this.parameters_.dialogButtonOpenId = parameters['dialogOpenButtonId'] || 'show_dialog';
-  this.parameters_.dialogButtonCloseId = parameters['dialogCloseButtonId'] || 'close_dialog';
+  this.menubarId_ = parameters['menubarId'] || 'menubar';
+  this.buttonsId_ = parameters['controlButtonsId'] || 'buttons';
   
-  this.initializeThemeSwitcher_();
-  this.initializeDialog_();
+  this.initThemeSwitcher_();
+  this.initAboutDialog_();
+  this.initCommands_();
+  this.initMenubar_();
+  this.initButtons_();
+  this.connectListeners_();
 };
 
-showcase.prototype.initializeThemeSwitcher_ = function() {
+showcase.prototype.initThemeSwitcher_ = function() {
   new wtk.ThemeSwitcher();
 };
 
-showcase.prototype.initializeDialog_ = function() {
-  var diag = new wtk.Dialog();
-  diag.setPosition(100, 100);
-  diag.render();
-  diag.setTitle('Demo Dialog');
-  diag.setContent('<p>This is the inner content of the Dialog</p>');
-  
-  var open_button = new goog.ui.Button();
-  open_button.decorate(goog.dom.getElement(this.parameters_.dialogButtonOpenId));
-
-  var close_button = new goog.ui.Button();
-  close_button.decorate(goog.dom.getElement(this.parameters_.dialogButtonCloseId));
-  close_button.setEnabled(false);
-
-  goog.events.listen(open_button, goog.ui.Component.EventType.ACTION, function(){
-    open_button.setEnabled(false);
-    close_button.setEnabled(true);
-    diag.open();
-  });
-
-  goog.events.listen(close_button, goog.ui.Component.EventType.ACTION, function(){
-    close_button.setEnabled(false);
-    open_button.setEnabled(true);
-    diag.close();
-  });
+showcase.prototype.initCommands_ = function() {
+  this.saveCommand_ = new wtk.util.Command();
+  this.aboutCommand_ = new wtk.util.Command();
 };
+
+showcase.prototype.initMenubar_ = function() {
+  this.menubar_ = new wtk.menubar.Menubar();
+  this.menubar_.render(goog.dom.getElement(this.menubarId_));
+  
+  var fileMenu = new wtk.menubar.Menu('File');
+  this.menubar_.addMenu(fileMenu);
+  
+  var editMenu = new wtk.menubar.Menu('Edit');
+  this.menubar_.addMenu(editMenu);
+       
+  var viewMenu = new wtk.menubar.Menu('View');
+  this.menubar_.addMenu(viewMenu);
+  
+  var helpMenu = new wtk.menubar.Menu('Help');
+  this.menubar_.addMenu(helpMenu);
+  
+  var newRecord = new wtk.menubar.MenuItem('New', wtk.icon.NEW_WIN);
+  fileMenu.addItem(newRecord);
+  
+  var save = new wtk.menubar.MenuItem('Save', wtk.icon.DISK);
+  fileMenu.addItem(save);
+  this.saveCommand_.attachControl(save);
+  
+  var about  = new wtk.menubar.MenuItem('About', wtk.icon.INFO);
+  helpMenu.addItem(about);
+  this.aboutCommand_.attachControl(about);
+};
+
+showcase.prototype.initButtons_ = function() {
+  this.saveButton_ = new wtk.Button('Save');
+  this.saveButton_.render(goog.dom.getElement(this.buttonsId_));
+  this.saveCommand_.attachControl(this.saveButton_);
+  
+  this.deleteButton_ = new wtk.Button('Delete');
+  this.deleteButton_.render(goog.dom.getElement(this.buttonsId_));
+};
+
+showcase.prototype.initAboutDialog_ = function() {
+  this.aboutDiag_ = new wtk.Dialog();
+  this.aboutDiag_.setPosition(300, 100);
+  this.aboutDiag_.render();
+  this.aboutDiag_.enableModal(true);
+  this.aboutDiag_.setTitle('About WTK');
+  this.aboutDiag_.setContent('<p>Example and showcase of WTK</p>');
+};
+
+showcase.prototype.connectListeners_ = function() {
+  goog.events.listen(this.saveCommand_, wtk.util.Command.EventType.EXECUTE, this.handleSaveExe_, false, this);
+  goog.events.listen(this.aboutCommand_, wtk.util.Command.EventType.EXECUTE, this.handleAboutExe_, false, this);
+};
+
+showcase.prototype.handleSaveExe_ = function() {
+  alert('save')
+};
+
+showcase.prototype.handleAboutExe_ = function() {
+  this.aboutDiag_.open();
+};
+
 
 goog.exportSymbol('showcase', showcase);
